a495228bfe69705e0c9da26e3202c6d68332246e
Khushi89-K <khushikkhushik59@gmail.com>
2026-03-03 15:43:44 +0530

    comparision operator

---
+++ W3/D2/p4.js
@@ -0,0 +1,35 @@
+// // conditional statements
+// console.log("conditional statements");
+// let age=18;
+// if(age<13) {
+//     cnsole.log("Child");
+// }
+// else if(age<18){
+//     console.log("Teenager");
+// }
+// else {
+//     console.log("Adult");
+// }
+
+// // switch
+// console.log("switch statements");
+// const day = 3
+// switch(day){
+//     case "monday":
+//         console.log("start of the week");
+//         break;
+//     case "Wednesday":
+//         console.log("Mid of the week");
+//         break;
+//     case "friday":
+//         console.log("end of the work week");
+//         break;
+
+//     default:
+//         console.log("some day in the week");
+//         break;           
+// }
+
+// type conversion
+ let n = Number("ABC");
+ console.log( "n: ", n ,"type of n:", typeof n, "isNaN", isNaN(n));
