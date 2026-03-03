67fe0cc15d4f34d0792acc5084e6f0bf3c0d5afd
Khushi89-K <khushikkhushik59@gmail.com>
2026-03-03 15:42:30 +0530

    comparasion operator

---
+++ W3/D2/p2.js
@@ -0,0 +1,11 @@
+// comparasion opearator
+console.log("comparasion operator");
+let x=2, y=3;
+console.log("x==y: ", x==y); //loose equality
+console.log("x===y: ", x===y); // strict equality
+console.log("x!=y: ", x!=y);  //loose inequality
+console.log("x!==y: ", x!==y); // Strict inequality
+console.log("x>3y: ", x>y);
+console.log("x<y: ", x<y); 
+console.log("x>=y: ", x>=y);
+console.log("x<=y: ", x<=y);   
