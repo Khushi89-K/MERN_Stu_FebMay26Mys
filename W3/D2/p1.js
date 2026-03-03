4bd83d6daaff3e0e63191eaf39246296e6bb2c42
Khushi89-K <khushikkhushik59@gmail.com>
2026-03-03 15:41:49 +0530

    arithmetic operator

---
+++ W3/D2/p1.js
@@ -0,0 +1,14 @@
+// arithmetic operator
+console.log("arithmetic operator");
+const a=2, b=3;
+let c=0;
+console.log("a+b+c: ", c=a+b);
+console.log("a-b: ", a-b);
+console.log("a*b: ", a*b);
+console.log("a/b: ", a/b);
+console.log("a%b: ", a%b);
+console.log("a**b: ", a**b)
+// console.log("a++: ", a++); //post increment
+// console.log("++a: ", ++a); //pre increment
+// console.log("a--: ", a--); //post decrement
+// console.log("--a: ", --a); //pre decrement
