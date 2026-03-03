3c6870aca0ec9970455e39a59ff18cd3186021c7
Khushi89-K <khushikkhushik59@gmail.com>
2026-03-03 15:42:55 +0530

    logical  operator

---
+++ W3/D2/p3.js
@@ -0,0 +1,6 @@
+// logical operators
+console.log("Logical operators");
+let a=true, b=false;
+console.log("a&&b= ", a&&b); //Logical AND
+console.log("a||b= ", a||b); // Logical OR
+console.log("!b= ", !b); // Logical NOT 
