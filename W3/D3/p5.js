dee1d11f953b29c7f0f22979ef377f0671a07585
Khushi89-K <khushikkhushik59@gmail.com>
2026-03-04 13:29:39 +0530

    basics of object

---
+++ W3/D3/p5.js
@@ -0,0 +1,5 @@
+//  basics of Objects
+ const person ={name:'khushi', age:'22', isStudent: false};
+console.log("person", person);
+console.log("Name",person.name);
+console.log("Age:", person['age']);
