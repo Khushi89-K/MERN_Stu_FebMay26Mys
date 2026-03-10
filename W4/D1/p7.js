f85033fcf314b6e80906edf6d846bc331b364728
Khushi89-K <khushikkhushik59@gmail.com>
2026-03-10 09:08:44 +0530

    query selector

---
+++ W4/D1/p7.js
@@ -0,0 +1,35 @@
+// getElementById
+console.log("Document Object:",document);
+console.log("Page title",document.title);
+
+// const heading = document.getElementById("mainHeading");
+// console.log("Heading text",heading.textContent);
+
+// getElementByClassName
+const info = document.getElementsByClassName("info");
+const run = document.getElementById("run");
+
+// getElementByTagName
+const spanTag = document.getElementsByTagName("span");
+run.addEventListener("click",function(){
+    for(let i=0;i<info.length;i++){
+        console.log(info[i].textContent);
+        info[i].style.color = "purple";
+    }
+
+    for(let i=0;i<spanTag.length;i++){
+        spanTag[i].style.backgroundColor = "pink";
+    }
+});
+
+//Query Selector:returns the first element
+//matching a CSS selector
+const mainFirstHeading = document.querySelector(".mainHeading");
+mainFirstHeading.style.color = "red";
+
+// query selector all: returns all elements matching the selector
+const tasks = document.querySelectorAll(".task");
+// tasks.style.color='purple';
+tasks.forEach(function(tasks){
+    tasks.style.color='purple';
+});
