92c9a72cac82012db24240f3f353ee2ec937b975
Khushi89-K <khushikkhushik59@gmail.com>
2026-03-16 10:53:15 +0530

    wallets

---
+++ Assignments/JSFunctionsLoops/assignment5.js
@@ -0,0 +1,14 @@
+const wallets = {
+  owner: "khushi",
+  balance:300,
+  lastTransaction: null,
+
+  deposit: function(amount) { 
+  },
+  withdraw: function(amount) {
+  }
+};
+wallets.deposit(500);
+wallets.withdraw(30);
+
+console.log(wallets);
